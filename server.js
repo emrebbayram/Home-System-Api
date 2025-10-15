07c26e1c161e50d0c395177284f8482c1864a013
emrebbayram <slican24@gmail.com>
2025-10-15 11:39:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,36 +2,10 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-function dataJsonCheck (){
-    fs.readFile("/data/data.json","utf8",(err,data) => {
-        if (err){
-            console.log(err)
-            console.log("/data/data.json oluşturuluyor...");
-            var data = 
-            [
-                {
-                    name:'Bob',
-                    pass:"0123456789rds",
-                    email:"bob@dsa.com",
-                    permissionDate:"2022-02-02"
-                },
-                {
-                    name:'Bobsd',
-                    pass:"0123456s789rds",
-                    email:"bob@dsa.com",
-                    permissionDate:"2022-02-02"
-                }
-            ]
-            fs.writeFile("/data/data.json",JSON.stringify(data),(err) => {
-                if (err){
-                    console.log(err)
-                }
-            })
-        }
-    });
+function logger(req,res,next){
+    console.log(req.get("host"))
+    next()
 }
-
-dataJsonCheck()
 
 app.use(express.json())
 
@@ -49,13 +23,8 @@ app.get("/contact",(req,res) => {
 
 
 const userRouter = require("./router/users")
-
-function logger(req,res,next){
-    console.log(req.get("host"))
-    next()
-}
-
 const acRouter = require("./router/ac")
+
 app.use("/ac",acRouter)
 
 app.use("/users",userRouter)
