805bf18048ba609baf311cec1fdeb11ded7016a1
emrebbayram <slican24@gmail.com>
2025-10-15 11:50:38 +0300

    Update ac.js

--- router/ac.js
+++ router/ac.js
@@ -2,19 +2,21 @@ const express = require("express")
 const router = express.Router()
 
 const commands = Object.freeze({
-    START : 0,
-    STOP : 1,
-    RESTART : 2,
-    TEMP_INCREASE : 3,
-    TEMP_DECREASE : 4
+    START: 0,
+    STOP: 1,
+    RESTART: 2,
+    TEMP_INCREASE: 3,
+    TEMP_DECREASE: 4
 })
 
 var current_command;
 
-router.get("/",(req,res) => {
+router.get("/", (req, res) => {
     res.status(200).send("AC is working")
 })
-router.get("/command",(req,res) => {
-    res.status(200).send(""+current_command)
+
+router.get("/command", (req, res) => {
+    res.status(200).send("" + current_command)
 })
 
+module.exports = router; // <-- BU SATIR ZORUNLU
