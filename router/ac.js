5d772cf53311c1d87d98aca6872b1b8705fcf0b0
emrebbayram <slican24@gmail.com>
2025-10-16 18:54:40 +0300

    Update ac.js

--- router/ac.js
+++ router/ac.js
@@ -15,8 +15,23 @@ const commands = Object.freeze({
 
 var current_command;
 
+var last_temp_check;
+
+var lastTemp;
+
 function GetCommandName(value) {
   return Object.keys(commands).find(key => commands[key] === value);
+}
+
+async function waitForTempUpdate(old_temp_check) {
+    return new Promise((resolve) => {
+        const interval = setInterval(() => {
+            if (old_temp_check !== last_temp_check) {
+                clearInterval(interval);
+                resolve();
+            }
+        }, 100);
+    })
 }
 
 router.get("/", (req, res) => {
@@ -33,13 +48,24 @@ router.get("/command", (req, res) => {
 router.get("/commands", (req, res) => {
     res.status(200).send(commands)
 })
-router.post("/command", (req, res) => {
+router.post("/command", async (req, res) => {
     const command = commands[req.query.command]
     if (command === undefined) {
         res.status(400).send("Invalid command")
     }else {
         current_command = command
-        res.status(200).send("Command received: " + command)
+        if (command === commands.GET_TEMP) {
+            if (last_temp_check === undefined || (Date.now() - last_temp_check) > 300000) {
+                const old_temp_check = last_temp_check;
+                await waitForTempUpdate(old_temp_check);
+                var data;
+                data["temp"] = lastTemp;
+                res.status(200).send(data);
+            }
+            res.status(200).send("Command received: " + command)
+        }else {
+            res.status(200).send("Command received: " + command)
+        }
     }
 })
 router.post("/temp/response", (req, res) => {
@@ -47,7 +73,8 @@ router.post("/temp/response", (req, res) => {
     if (temp === undefined) {
         res.status(400).send("Invalid temperature")
     }else {
-        current_command = temp
+        lastTemp = temp;
+        last_temp_check = Date.now();
         res.status(200).send(temp)
     }
 })
